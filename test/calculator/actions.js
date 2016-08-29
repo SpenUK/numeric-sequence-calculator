a3189a8049ea78885b93ed981d98d2e72b666e56
Spen Taylor <spen_@hotmail.co.uk>
2016-08-29 22:52:41 +1000

    Actions tests now passing

--- test/calculator/actions.js
+++ test/calculator/actions.js
@@ -1,19 +1,22 @@
 import {expect} from 'chai'
-import {actions} from '../../src/calculator/actions'
+import {
+	calculateSequences,
+	inputUpdate
+} from '../../src/calculator/actions'
 
 describe('.calculateSequences', () => {
 	it('returns an object with the type of CALCULATE_SEQUENCES', () => {
-		expect(actions.calculateSequences().type).toEqual('CALCULATE_SEQUENCES')
+		expect(calculateSequences().type).to.eql('CALCULATE_SEQUENCES')
 	})
 })
 
 describe('.inputUpdate', () => {
 	it('returns an object with the type of CALCULATOR_INPUT_UPDATE', () => {
-		expect(actions.calculateSequences().type).toEqual('CALCULATOR_INPUT_UPDATE')
+		expect(inputUpdate().type).to.eql('CALCULATOR_INPUT_UPDATE')
 	})
 
-	it('returns an object with a "value" property equal to an input argument', () => {
+	it('returns an object with a "value" property equal to the input argument', () => {
 		const argument = 'argument'
-		expect(actions.calculateSequences(argument).type).toEqual(argument)
+		expect(inputUpdate(argument).value).to.eql(argument)
 	})
 })
