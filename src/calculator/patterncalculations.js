61cd2b7c480eb3267b7229b39869ae2b2bf881f7
Spen Taylor <spen_@hotmail.co.uk>
2016-08-29 22:36:25 +1000

    All patterns now passing tests

--- src/calculator/patterncalculations.js
+++ src/calculator/patterncalculations.js
@@ -33,9 +33,39 @@ export const getOdds = n => {
 }
 
 export const getLetters = n => {
-	return n
+	let sequence = []
+
+	for (let i = 0; i <= n; i++) {
+		const outputC = (i % 3 === 0)
+		const outputE = (i % 5 === 0)
+		const outputZ = outputC && outputE
+
+		sequence.push(
+			outputZ ? 'Z' : 
+			outputC ? 'C' : 
+			outputE ? 'E' : i
+		)
+	}
+
+	return sequence
 }
 
 export const getFibonnaci = n => {
-	return n
+	const sequence = [0]
+
+	if (n > 0) {
+		sequence[1] = 1
+	}
+
+	for (let i = 2; true; i++) {
+		const next = sequence[i-2] + sequence[i-1]
+
+		if (next <= n) {
+			sequence[i] = next
+		} else {
+			break	
+		}
+	}
+
+	return sequence
 }
