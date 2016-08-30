73fe58ca767a57c93c55c1764c17db7eb1e00ad0
Spen Taylor <spen_@hotmail.co.uk>
2016-08-30 10:36:04 +1000

    Reducer logic now handling invalid inputs as in tests

--- src/calculator/reducer.js
+++ src/calculator/reducer.js
@@ -14,6 +14,7 @@ import {
 const initialState = {
 	inputValue: 6,
 	error: null,
+	hasResults: false,
 	results: {
 		evens: null,
         fibonnaci: null,
@@ -36,12 +37,32 @@ export function CalculatorReducer (state = initialState, action) {
 		}
 
 		case CALCULATE_SEQUENCES: {
-	    	const inputValue = state.inputValue
-	    	const sequencial = getSequence(inputValue).join(' ')
-	    	const evens = getEvens(inputValue).join(' ')
-	    	const odds = getOdds(inputValue).join(' ')
-	    	const letters = getLetters(inputValue).join(' ')
-	    	const fibonnaci = getFibonnaci(inputValue).join(' ')
+	    	const inputValue = parseFloat(state.inputValue)
+	    	let sequencial
+			let evens
+			let odds
+			let letters
+			let fibonnaci
+
+			if (parseInt(inputValue) !== inputValue) {
+				return Object.assign({}, state, {
+					error: 'Input must be a whole number',
+					hasResults: false,
+					results: {
+						evens: null,
+				        fibonnaci: null,
+				        letters: null,
+				        odds: null,
+				        sequencial: null,
+					}
+				})
+			}
+
+	    	sequencial = getSequence(inputValue).join(' ')
+	    	evens = getEvens(inputValue).join(' ')
+	    	odds = getOdds(inputValue).join(' ')
+	    	letters = getLetters(inputValue).join(' ')
+	    	fibonnaci = getFibonnaci(inputValue).join(' ')
 	    		
 			return Object.assign({}, state, {
 				hasResults: true,
